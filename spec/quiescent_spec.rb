a1b3b807d30b4f05da3065609647576374c39cca
Will Benton <willb@redhat.com>
2011-08-29 17:10:00 -0500

    Initial revision of quiescent (smashed from WIP branch).

--- spec/quiescent_spec.rb
+++ spec/quiescent_spec.rb
@@ -1,7 +1,68 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe "Quiescent" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+describe Quiescent do
+  it "successfully creates quiescents with default arguments" do
+    classid = gen_id("Foo")
+    k = Klass(classid, "include Quiescent", QConst("Bar", "12345"))
+    eval(k.to_s)
+    Kernel.const_get(classid)::Bar.should == 12345
   end
+
+  it "successfully creates multiple quiescents with default arguments in a single class" do
+    classid = gen_id("Foo")
+    k = Klass(classid, "include Quiescent", QConst("Fred", ":a"), QConst("Barney", "54321"))
+    eval(k.to_s)
+    Kernel.const_get(classid)::Fred.should == :a
+    Kernel.const_get(classid)::Barney.should == 54321
+  end
+
+  it "allows assigning alternate values to quiescents with default arguments" do
+    classid = gen_id("Foo")
+    k = Klass(classid, "include Quiescent", QConst("Bar", "12345"))
+    eval(k.to_s)
+    Kernel.const_get(classid).Bar = 54321
+    Kernel.const_get(classid)::Bar.should == 54321
+  end
+
+  it "allows assigning alternate values to quiescents with default arguments only once" do
+    classid = gen_id("Foo")
+    k = Klass(classid, "include Quiescent", QConst("Bar", "12345"))
+    eval(k.to_s)
+    Kernel.const_get(classid).Bar = 54321
+    lambda {Kernel.const_get(classid).Bar = 12345}.should raise_error
+    Kernel.const_get(classid)::Bar.should == 54321    
+  end
+
+  it "successfully creates quiescents with block arguments" do
+    classid = gen_id("Foo")
+    k = Klass(classid, "include Quiescent", QConst("Bar", Do("12345")))
+    eval(k.to_s)
+    Kernel.const_get(classid)::Bar.should == 12345
+  end
+  
+  it "only evaluates quiescent block arguments once" do
+    $foo = Object.new
+    $foo.should_receive(:hash).once
+    classid = gen_id("Foo")
+    k = Klass(classid, "include Quiescent", QConst("Bar", Do("$foo.hash; 12345")))
+    eval(k.to_s)
+    Kernel.const_get(classid)::Bar.should == 12345
+    Kernel.const_get(classid)::Bar
+  end
+
+  it "only adds quiescents to the classes in which they are declared" do
+    classid1 = gen_id("Fred")
+    classid2 = gen_id("Barney")
+    [Klass(classid1, "include Quiescent", QConst("Bar", "12345")),
+     Klass(classid2, "include Quiescent", QConst("Foo", "54321"))].each do |k|
+       eval(k.to_s)
+    end
+    
+    Kernel.const_get(classid1)::Bar.should == 12345
+    Kernel.const_get(classid2)::Foo.should == 54321
+
+    lambda {Kernel.const_get(classid1)::Foo}.should raise_error
+    lambda {Kernel.const_get(classid2)::Bar}.should raise_error
+  end
+
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,10 +2,68 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'rspec'
 require 'quiescent'
+require 'erb'
+require 'digest/md5'
 
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
+
+SClass = Struct.new(:name, :decls)
+SQConst = Struct.new(:name, :default, :block)
+SDo = Struct.new(:body)
+
+class SDo
+  def to_s
+    "do ; #{body} ; end"
+  end
+end
+
+class SQConst
+  def to_s
+    "quiescent #{name.to_sym.inspect}#{(", " + default) if default} #{block if block}"
+  end
+end
+
+class SClass
+  TEMPLATE = ERB.new <<-END
+  class <%= name %>
+<% decls.each do |decl| %>    <%= decl.to_s %>
+<% end %>
+  end
+END
+  def to_s
+   TEMPLATE.result(binding)
+  end
+end
+
+def Klass(name, *decls)
+  SClass.new(name, decls)
+end
+
+def QConst(name, *values)
+  default, block = nil
+
+  if values.size == 1
+    if values[0].is_a?(SDo)
+      block = values[0]
+    else
+      default = values[0]
+    end
+  elsif values.size == 2
+    default, block = values
+  end
+  
+  SQConst.new(name, default, block)
+end
+
+def Do(body)
+  SDo.new(body)
+end
+
+def gen_id(base)
+  "#{base}_#{Digest::MD5.hexdigest(Time.now.iso8601(11))}"
+end
 
 RSpec.configure do |config|
   
